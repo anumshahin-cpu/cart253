8a79fc40f77481122b50f50e854d37eefb41729e
anumshahin-cpu <anumshahin06@gmail.com>
2026-09-17 16:30:09 -0400

    We created the landscape, then by accident, we did in the wrong program. Oops, our bad.

--- topics/challenges/instructions-challenge/template-p5-project/js/script.js
+++ topics/challenges/instructions-challenge/template-p5-project/js/script.js
@@ -1,24 +1,76 @@
 /**
- * Title of Project
- * Author Name
+ * Amazing Fantastic Beautiful Spectacular Lovely Perfect Heart Fluttering Nice Landscape 
+ * Alissa Horqque Antony Hatem Anum Shahin
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Not true, here's our description right in the flesh. 
+ * Grade us please.
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creating the canvas
 */
-function setup() {
 
+function setup() {
+  createCanvas(400, 400);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Declaring the landscape
 */
 function draw() {
+  background(7, 61, 141);
 
+  noStroke();
+
+  drawLandscape();
+} 
+
+/**
+ * Creating each part of this drawing
+*/
+function drawLandscape(){
+  drawGround();
+  drawMoon();
+  drawTree();
+  drawTent();
+  drawTentEntrance();
+}
+
+function drawGround(){
+  push();
+  fill(3, 18, 41);
+  rect(-200,320,800,400);
+  pop();
+}
+
+function drawMoon(){
+  push();
+  fill(255, 255, 204);
+  circle(100, 100, 100);
+  pop();
+}
+
+function drawTree(){
+  push();
+  fill (3,18,41);
+  triangle(105,325,180,325,140,230)
+  pop();
+}
+
+function drawTent(){
+  push();
+  fill (153,76,0);
+  //triangle(300,400, 400, 345, 350, 230)
+  triangle(210,345, 380, 345, 300, 230)
+  pop();
+}
+
+function drawTentEntrance(){
+  push();
+  fill(3,18,41);
+  ellipse (300, 350, 80, 100)
+  pop();
 }
